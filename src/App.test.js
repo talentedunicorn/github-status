8435542a88ae810e67e1d0fc29f9a37839ee28fa
Talentedunicorn <dev@talentedunicorn.com>
2020-05-13 23:53:42 +0800

    setup tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render, waitForElement} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(() => {
+  global.fetch = jest.fn();
+});
+
+test('should display github status', async () => {
+  global.fetch = () => Promise.resolve({status: 200, text: 'up'});
+  const {getByTestId, getByText} = await waitForElement(() => render(<App />));
+  expect(getByTestId('status')).toBeInTheDocument();
+});
+
+test('should display github status time', async () => {
+  global.fetch = () => Promise.reject({status: 400, text: 'down'});
+  const {getByTestId} = await waitForElement(() => render(<App />));
+  expect(getByTestId('status-time')).toBeInTheDocument();
 });
